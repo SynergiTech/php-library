54dd0d7d3470c3b21320ab0cae40a9fda15dc383
William Renfrew <william@synergitech.co.uk>
2018-09-29 15:19:13 +0100

    Revert "Include method for Authentication"
    This reverts commit d73bdd435acd14e6522e896f43ec0a67b71f8438.

--- src/Sirportly.php
+++ src/Sirportly.php
@@ -1,4 +1,8 @@
 <?php
+namespace Sirportly;
+
+use Sirportly\Exceptions\SirportlyDefaultException;
+
 /**
 * Sirportly PHP API Library
 * @see https://github.com/sirportly/php-library
@@ -10,117 +14,142 @@ class Sirportly
     private $secret;
     private $url;
 
-    public function __construct($token, $secret, $url='https://api.sirportly.com')
+    public function __construct($token, $secret, $url = 'https://api.sirportly.com')
     {
         $this->token  = $token;
         $this->secret = $secret;
-        $this->url    = $url;
+        $this->url = $url;
     }
 
-    private function query($action, $postdata=array())
+    /**
+     * @param $action
+     * @param array $postdata
+     * @return mixed
+     * @throws SirportlyDefaultException
+     */
+    private function query($action, $postdata = array())
     {
-        $curl = curl_init();
+        $url = $this->url . $action;
+
+        $header = array('X-Auth-Token: ' . $this->token, 'X-Auth-Secret: ' . $this->secret);
+
         $query_string = "";
-        foreach ($postdata as $k=>$v) {
-            $query_string .= "$k=".urlencode($v)."&";
+        foreach ($postdata as $k => $v) {
+            $query_string .= "$k=" . urlencode($v) . "&";
         }
-        $header = array('X-Auth-Token: '.$this->token, 'X-Auth-Secret: '.$this->secret);
+
+        $curl = curl_init();
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_URL, $this->url.$action);
+        curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_BUFFERSIZE, 131072);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $query_string);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($curl, CURLOPT_POST, 1);
 
         $result = curl_exec($curl);
+        $info = curl_getinfo($curl);
+
+        if ($result === false || ! in_array($info['http_code'], [200, 201])) {
+            $error = "No cURL data returned for $url [http error: ". $info['http_code']. "]";
+            if (curl_error($curl)) {
+                $error .= "\n" . curl_error($curl);
+            }
+            if ($result !== false) {
+                $error .= "\n" . $result;
+            }
+            throw new SirportlyDefaultException($error);
+        }
+
         curl_close($curl);
+
         $decode = json_decode($result, true);
         return $decode;
     }
 
     public function ticket($ticket_reference)
     {
-        return $this->query('/api/v1/tickets/ticket', array('reference' => $ticket_reference));
+        return $this->query('/api/v2/tickets/ticket', array('ticket' => $ticket_reference));
     }
 
     public function create_ticket($params = array())
     {
-        return $this->query('/api/v1/tickets/submit', $params);
+        return $this->query('/api/v2/tickets/submit', $params);
     }
 
     public function post_update($params = array())
     {
-        return $this->query('/api/v1/tickets/post_update', $params);
+        return $this->query('/api/v2/tickets/post_update', $params);
     }
 
     public function tickets($page = '1')
     {
-        return $this->query('/api/v1/tickets/all', array('page' => $page));
+        return $this->query('/api/v2/tickets/all', array('page' => $page));
     }
 
     public function update_ticket($params = array())
     {
-        return $this->query('/api/v1/tickets/update', $params);
+        return $this->query('/api/v2/tickets/update', $params);
     }
 
     public function run_macro($params = array())
     {
-        return $this->query('/api/v1/tickets/macro', $params);
+        return $this->query('/api/v2/tickets/macro', $params);
     }
 
     public function add_follow_up($params = array())
     {
-        return $this->query('/api/v1/tickets/followup', $params);
+        return $this->query('/api/v2/tickets/add_followup', $params);
     }
 
     public function create_user($params = array())
     {
-        return $this->query('/api/v1/users/create', $params);
+        return $this->query('/api/v2/users/create', $params);
     }
 
     public function statuses()
     {
-        return $this->query('/api/v1/objects/statuses');
+        return $this->query('/api/v2/objects/statuses');
     }
 
     public function priorities()
     {
-        return $this->query('/api/v1/objects/priorities');
+        return $this->query('/api/v2/objects/priorities');
     }
 
     public function teams()
     {
-        return $this->query('/api/v1/objects/teams');
+        return $this->query('/api/v2/objects/teams');
     }
 
     public function brands()
     {
-        return $this->query('/api/v1/objects/brands');
+        return $this->query('/api/v2/objects/brands');
     }
 
     public function departments()
     {
-        return $this->query('/api/v1/objects/departments');
+        return $this->query('/api/v2/objects/departments');
     }
 
     public function escalation_paths()
     {
-        return $this->query('/api/v1/objects/escalation_paths');
+        return $this->query('/api/v2/objects/escalation_paths');
     }
 
     public function slas()
     {
-        return $this->query('/api/v1/objects/slas');
+        return $this->query('/api/v2/objects/slas');
     }
 
     public function filters()
     {
-        return $this->query('/api/v1/objects/filters');
+        return $this->query('/api/v2/objects/filters');
     }
 
     public function spql($params = array())
     {
-        return $this->query('/api/v1/tickets/spql', $params);
+        return $this->query('/api/v2/tickets/spql', $params);
     }
 
     /**
@@ -129,7 +158,7 @@ class Sirportly
     */
     public function kb_list()
     {
-        return $this->query('/api/v1/knowledge/list');
+        return $this->query('/api/v2/knowledge/list');
     }
 
     /**
@@ -139,14 +168,14 @@ class Sirportly
     */
     public function kb($kb_id)
     {
-        return $this->query('/api/v1/knowledge/tree', array('kb' => $kb_id));
+        return $this->query('/api/v2/knowledge/tree', array('kb' => $kb_id));
     }
 
     /**
     * Fetch a list of users from your account
     * @return array        The users as an array.
     */
-    public function users($page=1)
+    public function users($page = 1)
     {
         return $this->query('/api/v2/users/all', array('page' => $page));
     }
@@ -155,17 +184,8 @@ class Sirportly
     * Fetch a list of contacts from your account
     * @return array        The contacts as an array.
     */
-    public function contacts($page=1)
+    public function contacts($page = 1)
     {
         return $this->query('/api/v2/contacts/all', array('page' => $page));
-    }
-
-    /**
-    * Authenticate a user for passthru authentication to a Public Support Centre
-    * @return array        The details for the support centre redirection
-    */ 
-    public function authentication($params)
-    {
-        return $this->query('/api/v1/authentication/support_centre_token', $params);
     }
 }
